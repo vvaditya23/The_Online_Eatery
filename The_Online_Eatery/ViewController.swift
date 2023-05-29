295293b7c6e71a2d104a3f1c82db2753a811897e
vvaditya23 <vvaditya23@icloud.com>
2023-05-29 11:34:30 +0530

    Initial Commit

---
+++ The_Online_Eatery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The_Online_Eatery
+//
+//  Created by ヴィヤヴャハレ・アディティア on 29/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
